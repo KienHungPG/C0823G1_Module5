abb69388ea1e54b7d7424ab42f3111b4ad8a8820
KienHungPG <voxkienhung@gmail.com>
2024-01-24 14:40:29 +0700

    up ss2

---
+++ ss2_reactjs_jsx/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from './components/Student';
+import SignIn from './components/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <Student/>
+      <SignIn/>
+    </div>
+  );
+}
+
+export default App;
